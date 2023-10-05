8b3b0fa7d3982c8e64d74459f20a26d50e79ccf4
Shahid Khan <shahidseran786@gmail.com>
2023-10-05 21:35:34 +0530

    Pranay name added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <section className='relative bg-black text-center text-white py-4'>
-      <span className='font-bold'>Made with ❤️ by Shahid</span>
+      <span className='font-bold'>Made with ❤️ by Shahid & Pranay</span>
     </section>
   )
 }
